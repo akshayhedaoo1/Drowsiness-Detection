6666d3d2d7660183472162a986cb0d36d1df1a35
Akshay Hedaoo <akshayhedaoo1@gmail.com>
2022-01-14 12:58:41 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,28 +7,25 @@ import os
 from pygame import mixer
 import cv2
 import numpy as np
-import streamlit as st
-
-
 
 class VideoTransformer(VideoTransformerBase):
 
     def __init__(self):
         
-        self.face = cv2.CascadeClassifier(r'haar cascade files/haarcascade_frontalface_alt.xml')
-        self.leye = cv2.CascadeClassifier(r'haar cascade files/haarcascade_lefteye_2splits.xml')
-        self.reye = cv2.CascadeClassifier(r'haar cascade files/haarcascade_righteye_2splits.xml')
+        self.face = cv2.CascadeClassifier(r'/home/akshayhedaoo/Desktop/Drowsiness_Detection/haar cascade files/haarcascade_frontalface_alt.xml')
+        self.leye = cv2.CascadeClassifier(r'/home/akshayhedaoo/Desktop/Drowsiness_Detection/haar cascade files/haarcascade_lefteye_2splits.xml')
+        self.reye = cv2.CascadeClassifier(r'/home/akshayhedaoo/Desktop/Drowsiness_Detection/haar cascade files/haarcascade_righteye_2splits.xml')
 
-        self.model = load_model('Model.h5')
+        self.model = load_model(r'/home/akshayhedaoo/Desktop/Drowsiness_Detection/Model.h5')
         self.font = cv2.FONT_HERSHEY_PLAIN
+        self.path = r'/home/akshayhedaoo/Desktop/Drowsiness_Detection'
         self.score = 0
+        mixer.init()
+        self.sound = mixer.Sound('/home/akshayhedaoo/Desktop/Drowsiness_Detection/alarm.wav')
     
     def transform(self, frame):
         frame = frame.to_ndarray(format="bgr24")
         height, width = frame.shape[:2]
-        
-        mixer.init()
-        sound = mixer.Sound('alarm.wav')
 
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -39,19 +36,19 @@ class VideoTransformer(VideoTransformerBase):
         for (x,y,w,h) in faces:
             cv2.rectangle(frame, (x,y), (x+w, y+h), (128, 128, 128), 1)
     
-        for (x,y, w,h) in l_eye:
-            l_eye = frame[y:y+h,x:x+w]
-            l_eye = cv2.cvtColor(l_eye, cv2.COLOR_BGR2RGB)
-            l_eye = cv2.resize(l_eye, (32, 32))
-            l_eye = np.expand_dims(l_eye, axis = 0)
-            lpred = self.model.predict(l_eye)
-        
         for (x,y, w,h) in r_eye:
-            r_eye = frame[y:y+h,x:x+w]
+            r_eye = frame[y:y+h,x:x+h]
             r_eye = cv2.cvtColor(r_eye, cv2.COLOR_BGR2RGB)
             r_eye = cv2.resize(r_eye, (32, 32))
             r_eye = np.expand_dims(r_eye, axis = 0)
             rpred = self.model.predict(r_eye)
+        
+        for (x,y, w,h) in l_eye:
+            l_eye = frame[y:y+h,x:x+h]
+            l_eye = cv2.cvtColor(l_eye, cv2.COLOR_BGR2RGB)
+            l_eye = cv2.resize(l_eye, (32, 32))
+            l_eye = np.expand_dims(l_eye, axis = 0)
+            lpred = self.model.predict(l_eye)
         
         if (np.argmax(lpred, axis =1)[0] == 0 and np.argmax(rpred, axis =1)[0] == 0):
             self.score += 1
@@ -65,7 +62,7 @@ class VideoTransformer(VideoTransformerBase):
         cv2.putText(frame, 'Score :'  +str(self.score), (100, height), self.font, 1, (0, 0, 255), 1, cv2.LINE_8)       
         if (self.score > 15):
             try:
-                sound.play()
+                self.sound.play()
                 if self.score < 15:
                     mixer.pause()
                 else:
@@ -74,5 +71,5 @@ class VideoTransformer(VideoTransformerBase):
                 pass
             cv2.rectangle(frame, (0,0), (width, height), (0,0, 255), 5)
         return frame
-  
+
 webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
